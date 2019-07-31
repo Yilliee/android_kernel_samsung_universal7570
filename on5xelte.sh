77f2325771da6957fbc1da9f1ae334987a93f858
resurrected21 <prabhjot.sookshminfo@gmail.com>
2019-07-31 10:49:05 +0000

    pie-swa

--- on5xelte.sh
+++ on5xelte.sh
@@ -2,6 +2,6 @@
 
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=p
-export CROSS_COMPILE=~/toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/tc/bin/aarch64-linux-android-
 make on5xelte_defconfig
 make -j10
